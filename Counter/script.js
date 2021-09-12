f7f6797a481bf2d598d59dd7db033ac4bc433393
Ritu singh <87261019+RituSingh12@users.noreply.github.com>
2021-09-12 21:44:24 +0530

    Update script.js

--- Counter/script.js
+++ Counter/script.js
@@ -2,12 +2,13 @@ let num=document.getElementById("num");
 let increase=document.getElementById("increase");
 let decrease=document.getElementById("decrease");
 
+let n=0
+
 increase.addEventListener("click",function(){
     num.textContent=n-1;
     n++
 })
 
-let n=0
 decrease.addEventListener("click",function(){
         num.textContent=n-1;
         n--
